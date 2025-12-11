a9bcd5dac9d02ca708b3a804c67447595a03e973
Abdirahem <elskumar.am@gmail.com>
2025-12-11 20:22:32 +0800

    operator overloading

---
+++ op_overload.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class League
+{
+    int numberOfTeams;
+    public:
+    League(int teams) : numberOfTeams(teams) {};
+
+
+   
+//Declare the operator<< as a friend function
+    friend std::ostream& operator<<(std::ostream& os, const League& league);
+
+   
+};
+ std::ostream& operator<<(std::ostream& os, const League& league)
+    {
+        os << league.numberOfTeams;
+        return os;
+    }   
+
+int main()
+{
+    League premierLeague(20);
+    std::cout << "Premier League has " << premierLeague << " teams." << std::endl;
+    return 0;
+}
